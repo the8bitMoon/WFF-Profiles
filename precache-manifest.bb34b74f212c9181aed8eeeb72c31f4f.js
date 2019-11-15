a017298a16e8301aa4aa76d264b744a3719ab594
the8bitMoon <27790800+the8bitMoon@users.noreply.github.com>
2019-11-15 05:23:01 -0600

    Updates

---
+++ precache-manifest.bb34b74f212c9181aed8eeeb72c31f4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95a67dff24453ba837e1c95af94e7e1",
+    "url": "/WFF-Profiles/index.html"
+  },
+  {
+    "revision": "49bda800c09fa8390fd2",
+    "url": "/WFF-Profiles/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2770bb19c57ab7a79993",
+    "url": "/WFF-Profiles/static/js/2.8f04f743.chunk.js"
+  },
+  {
+    "revision": "49bda800c09fa8390fd2",
+    "url": "/WFF-Profiles/static/js/main.176e0e73.chunk.js"
+  },
+  {
+    "revision": "8b85b9913d38692fc868",
+    "url": "/WFF-Profiles/static/js/runtime-main.dfdf35c9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/WFF-Profiles/static/media/logo.25bf045c.svg"
+  }
+]);

e0966809a942b560e22dc7cae180aaed1c53b150
the8bitMoon <27790800+the8bitMoon@users.noreply.github.com>
2019-11-15 06:04:07 -0600

    Set up "under construction" landing page.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ 	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Worldwide Future Foundation Member Profiles</h1>
+				<p>Under Construction!</p>
+			</header>
+			{/* <main>
+				Profiles will go here.
+			</main> */}
+		</div>
+ 	);
+}
+
+export default App;
